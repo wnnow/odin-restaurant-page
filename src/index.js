cef37c0873b6d10fca3bb72c1355856c72ea8128
wnnow <117201813+wnnow@users.noreply.github.com>
2024-01-18 02:51:04 +0700

    Add updateComponet func

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createNavbar } from "./navbar";
 import { createHomeContent } from "./home";
 import { createMenuContent } from "./menu";
 import { createContactComponent } from "./contact";
+
 import "./style.css";
 
 function createContentDiv() {
@@ -34,15 +35,34 @@ function injectContent(createContentFunction) {
   mainContentContainer.appendChild(createContentFunction());
 }
 
-// injectContent(createHomeContent);
-// injectContent(createMenuContent);
-injectContent(createContactComponent);
+injectContent(createHomeContent);
 
-// function clearComponent() {
-//   const mainContentContainer = document.querySelector(
-//     "#main-content-container"
-//   );
-//   while (mainContentContainer.firstChild) {
-//     mainContentContainer.removeChild(mainContentContainer.firstChild);
-//   }
-// }
+function clearComponent() {
+  const mainContentContainer = document.querySelector(
+    "#main-content-container"
+  );
+  while (mainContentContainer.firstChild) {
+    mainContentContainer.removeChild(mainContentContainer.firstChild);
+  }
+}
+
+function updateComponent() {
+  const navbarChildren = Array.from(
+    document.querySelector(".navbar").querySelectorAll("*")
+  );
+  navbarChildren.forEach((child) => {
+    child.addEventListener("click", () => {
+      clearComponent();
+      if (child.classList.value === "home") {
+        injectContent(createHomeContent);
+      }
+      if (child.classList.value === "menu") {
+        injectContent(createMenuContent);
+      }
+      if (child.classList.value === "contact") {
+        injectContent(createContactComponent);
+      }
+    });
+  });
+}
+updateComponent();
